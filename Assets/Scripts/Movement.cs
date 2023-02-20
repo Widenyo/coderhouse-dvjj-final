a30fb5a2cb2d0f1bda2221a1844c496b32f1e8e2
butwithoutcontext <butwithoutcontext@gmail.com>
2023-02-20 16:43:59 -0300

    Feature: Added a Quicksave function

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,16 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class Savekeys
+{
+    public static string remainingHealthKey = "Remaining Health";
+    public static string positionKey = "Current Position";
+}
+
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private int m_maxHealth;
+    public int m_currentHealth;
     private bool canJump;
+
+    void SaveData()
+    {
+        Vector3 l_position = transform.position;
+        PlayerPrefs.SetInt(Savekeys.remainingHealthKey, m_currentHealth);
+        PlayerPrefs.SetFloat(Savekeys.positionKey, l_position.x);
+    }
+
+    void LoadData()
+    {
+        m_currentHealth = PlayerPrefs.GetInt(Savekeys.remainingHealthKey, m_maxHealth);
+        float l_x = PlayerPrefs.GetFloat(Savekeys.positionKey, 0);
+        transform.position = new Vector3(l_x, transform.position.y);
+    }
+
+    private void Awake()
+    {
+        m_currentHealth = m_maxHealth;
+    }
 
     void Update()
     {
         Schhmovin();
         checkFall();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SaveData();
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LoadData();
+        }
+    }
+
+    [ContextMenu ("Receive Damage")] void TakeDamage()
+    {
+        m_currentHealth -= 1;
     }
 
     void Schhmovin()
